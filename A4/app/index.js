ef449047f5812c8d7da6387728bcf1030772cc64
chenashley998 <37951084+chenashley998@users.noreply.github.com>
2023-11-24 13:15:53 -0800

    barebones layout added
    simple layout for homescreen/buildout added. no interactive (touchables)

--- A4/app/index.js
+++ A4/app/index.js
@@ -9,11 +9,7 @@ import {
   Dimensions,
   TouchableOpacity,
 } from "react-native";
-import {
-  useSpotifyAuth,
-  useSpotifyTracks,
-  millisToMinutesAndSeconds,
-} from "../utils";
+import { useSpotifyAuth, useSpotifyTracks } from "../utils";
 import { StatusBar } from "expo-status-bar";
 import { Images, Themes } from "../assets/Themes";
 
@@ -46,107 +42,67 @@ export default function App() {
     return artistList;
   }
 
-  let content = null;
-  if (token && tracks) {
-    content = (
-      <>
-        <View style={styles.topBar}>
-          <View style={styles.topBarContainer}>
-            <Image source={Images.spotify} style={styles.tracksLogo} />
-            <Text style={styles.tracksText}>My Top Tracks</Text>
-          </View>
-        </View>
+  let fitCastBagItems = null;
+  fitCastBagItems = (
+    <>
+      <Image style={styles.fitCastBagItem}></Image>
+    </>
+  );
 
-        <FlatList
-          style={styles.flatList}
-          data={tracks}
-          renderItem={({ item, index }) => (
-            <>
-              <StatusBar style="light" />
-              <View style={styles.songContainer}>
-                <TouchableOpacity>
-                  <Link
-                    href={{
-                      pathname: "./songPreview",
-                      params: {
-                        previewUrl: item.previewUrl,
-                      },
-                    }}
-                    asChild
-                  >
-                    <Ionicons
-                      name="play-circle"
-                      size={30}
-                      color={Themes.colors.spotify}
-                    />
-                  </Link>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <Link
-                    style={styles.songDetailLink}
-                    href={{
-                      pathname: "/songDetail",
-                      params: {
-                        externalUrl: item.externalUrl,
-                      },
-                    }}
-                    // asChild
-                  >
-                    <View style={styles.rightSongContainer}>
-                      <Image
-                        source={{
-                          uri: item.imageUrl,
-                        }}
-                        style={styles.coverImg}
-                      />
-                      <View style={styles.songInfoContainer}>
-                        <Text style={styles.songTitle} numberOfLines={1}>
-                          {item.songTitle}
-                        </Text>
-                        <Text style={styles.songArtist} numberOfLines={1}>
-                          {createArtistList(item.songArtists)}
-                        </Text>
-                      </View>
-                      <View style={styles.songAlbumContainer}>
-                        <Text style={styles.songAlbum} numberOfLines={1}>
-                          {item.albumName}
-                        </Text>
-                      </View>
-                      <Text style={styles.songLength}>
-                        {millisToMinutesAndSeconds(item.duration)}
-                      </Text>
-                    </View>
-                  </Link>
-                </TouchableOpacity>
-              </View>
-            </>
-          )}
-          keyExtractor={(item, index) => index.toString()}
-        />
-        <Stack.Screen options={{ header: () => null }} />
-        <StatusBar style="light" />
-      </>
-    );
-  } else {
-    content = (
-      <>
-        <StatusBar style="light" />
-        <SpotifyAuthButton />
-        <Stack.Screen options={{ header: () => null }} />
-      </>
-    );
-  }
+  let homescreen = (
+    <>
+      <View style={styles.weatherInfoContainer}>
+        <View style={styles.temperatureContainer}>
+          <Image style={styles.tempIcon}></Image>
+          <Text style={styles.tempText}></Text>
+        </View>
+        <Text style={styles.tempDescription}></Text>
+        <Text style={styles.tempHighLow}></Text>
+      </View>
+
+      <View style={styles.fitCastContainer}>
+        <Text style={styles.fitCastName}>Your FitCast</Text>
+        <View style={styles.fitCastOutfit}>
+          <Image style={styles.outfitTop}></Image>
+          <Image style={styles.outfitBottom}></Image>
+        </View>
+        <View style={styles.fitCastBag}>
+          <Image style={styles.fitCastBagOutline}>
+            <View style={styles.fitCastBagItems}>{fitCastBagItems}</View>
+          </Image>
+        </View>
+        <View style={styles.fitCastDescriptionContainer}>
+          <Text style={styles.fitCastDescriptionSummary}>
+            You've typically felt hot in this weather:
+          </Text>
+          <Text style={styles.fitCastDescriptionExtended}>
+            wear a t-shirt and shorts + pack an umbrella for the afternoon
+          </Text>
+        </View>
+      </View>
+    </>
+  );
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.phoneContainer}>
       <StatusBar style="light" />
-      {content}
+      <View style={backgroundImage}>
+        <View style={styles.topBar}>
+          <View style={styles.topBarContainer}>
+            <Image source={Images.spotify} style={styles.fitCastLogo} />
+            <Text style={styles.fitCastText}>My Top Tracks</Text>
+          </View>
+        </View>
+        {homescreen}
+        <Stack.Screen options={{ header: () => null }} />
+        <StatusBar style="light" />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  phoneContainer: {
     backgroundColor: Themes.colors.background,
     justifyContent: "center",
     alignItems: "center",
@@ -165,96 +121,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  tracksLogo: {
+  fitCastLogo: {
     // flex: 2,
     resizeMode: "contain",
     height: 25,
     width: 25,
   },
-  tracksText: {
+  fitCastText: {
     fontSize: 25,
     fontWeight: "bold",
     color: "white",
     paddingLeft: 10,
-  },
-
-  flatList: {
-    width: Dimensions.get("window").width,
-  },
-  authButton: {
-    backgroundColor: Themes.colors.spotify,
-    width: 250,
-    height: 40,
-    borderRadius: 30,
-  },
-  authButtonView: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-  },
-  authButtonLogo: {
-    flex: 2,
-    resizeMode: "contain",
-    height: 25,
-  },
-  authButtonText: {
-    flex: 8,
-    color: Themes.colors.white,
-    fontSize: 16,
-  },
-
-  songDetailLink: {
-    flex: 1,
-  },
-  songContainer: {
-    backgroundColor: Themes.colors.background,
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-    flexDirection: "row",
-    color: "white",
-    padding: 5,
-  },
-  rightSongContainer: {
-    backgroundColor: Themes.colors.background,
-    justifyContent: "center",
-    alignItems: "center",
-    width: 350,
-    flexDirection: "row",
-    color: "white",
-    padding: 5,
-  },
-  coverImg: {
-    resizeMode: "contain",
-    height: 70,
-    width: 70,
-  },
-  songInfoContainer: {
-    flex: 6,
-    flexDirection: "column",
-    marginRight: 5,
-    marginLeft: 10,
-  },
-  songTitle: {
-    color: "white",
-    fontWeight: "400",
-  },
-  songArtist: {
-    color: "white",
-    paddingTop: 1,
-    fontWeight: "200",
-  },
-  songAlbumContainer: {
-    flex: 4,
-  },
-  songAlbum: {
-    color: "white",
-  },
-  songLength: {
-    // flex: 1,
-    color: "white",
-    width: 35,
-    paddingLeft: 4,
   },
 });
