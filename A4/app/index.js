9a50cccf27ee68cdc59df9cfce2154f7beecc313
leonandsmiggles <leonmac@stanford.edu>
2023-12-03 15:09:30 -0800

    edits

--- A4/app/index.js
+++ A4/app/index.js
@@ -16,6 +16,7 @@ import BackgroundImage from "../assets/Images/dayBackground.jpg"; // Adjust the 
 import SunIcon from "../assets/Images/sunnyIconGreen.png"; // Adjust the path as per your folder structure
 import fitcast from "../assets/Images/fitcast.png"; // Adjust the path as per your folder structure
 import umbrella from "../assets/Images/umbrella.png";
+import pantsIcon from "../assets/Images/pantsIcon.png";
 import shirtIcon from "../assets/Images/shirtIcon.png";
 import shortsIcon from "../assets/Images/shortsIcon.png";
 import umbrellaIcon from "../assets/Images/umbrellaIcon.png";
@@ -31,6 +32,10 @@ import { Images, Themes } from "../assets/Themes";
 import { Link, Stack } from "expo-router/";
 import Ionicons from "@expo/vector-icons/Ionicons";
 const windowDimensions = Dimensions.get("window");
+
+const VerticalLine = () => {
+  return <View style={styles.line} />;
+};
 
 export default function App() {
   let fitCastBagItems = null;
@@ -64,13 +69,44 @@ export default function App() {
             <View style={styles.fitCastTitleContain}>
               <Text style={styles.fitCastName}>Your FitCast</Text>
             </View>
-            <View style={styles.fitCastIcons}>
-              <View style={styles.fitCastOutfit}>
-                <Image source={shirtIcon} style={styles.outfitTop}></Image>
-                <Image source={shortsIcon} style={styles.outfitBottom}></Image>
+            <View style={styles.items}>
+              <View style={styles.itemsToWear}>
+                <View style={styles.FitcastTextContainer}>
+                  <Text style={styles.suggestionTextNow}>Now: </Text>
+                </View>
+                <View style={styles.fitCastIcons}>
+                  <View style={styles.fitCastOutfit}>
+                    <Image source={shirtIcon} style={styles.outfitTop}></Image>
+                    <Text style={styles.textsymbols}> + </Text>
+                    <Image
+                      source={shortsIcon}
+                      style={styles.outfitBottom}
+                    ></Image>
+                  </View>
+                </View>
               </View>
-              <View style={styles.fitCastBag}>
-                <Image source={jacketUmbrellaIcon} style={styles.bag}></Image>
+              <VerticalLine />
+              <View style={styles.itemsToPack}>
+                <View style={styles.FitcastTextContainer1}>
+                  <Text style={styles.suggestionText}>For Later: </Text>
+                </View>
+                <View style={styles.fitCastOutfit}>
+                  <Image
+                    source={jacketIcon}
+                    style={styles.outfitOpacity}
+                  ></Image>
+                  <Text style={styles.textsymbols}> + </Text>
+                  <Image
+                    source={pantsIcon}
+                    style={styles.outfitOpacityPants}
+                  ></Image>
+                  <Text style={styles.textsymbols}> + </Text>
+
+                  <Image
+                    source={umbrellaIcon}
+                    style={styles.outfitOpacity}
+                  ></Image>
+                </View>
               </View>
             </View>
           </View>
@@ -112,18 +148,66 @@ export default function App() {
 const styles = StyleSheet.create({
   phoneContainer: {
     backgroundColor: "transparent", // Set background color to transparent
-    //backgroundColor: Themes.colors.background,
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
     width: windowDimensions.width,
     height: windowDimensions.height,
   },
+  FitcastTextContainer: { paddingLeft: "5%" },
+  FitcastTextContainer1: { paddingLeft: "0" },
+  suggestionTextNow: {
+    color: Themes.colors.logoGreen,
+    fontWeight: "bold",
+  },
+  suggestionText: {
+    color: Themes.colors.logoGreen,
+  },
+  itemsToWear: {
+    justifyContent: "space-between",
+    flexDirection: "column",
+    height: windowDimensions.height * 0.1,
+    width: "45%",
+    // backgroundColor: "white",
+    //borderRadius: "10%",
+    //borderColor: Themes.colors.logoGreen,
+    padding: 5,
+  },
+  itemsToPack: {
+    justifyContent: "space-between",
+    flexDirection: "column",
+    height: windowDimensions.height * 0.1,
+    width: "45%",
+    //backgroundColor: "white",
+    //borderRadius: "10%",
+    //borderColor: Themes.colors.logoGreen,
+    padding: 5,
+  },
   backgroundImage: {
     flex: 1,
     resizeMode: "cover", // or 'contain', 'stretch', etc.
     width: windowDimensions.width,
     height: windowDimensions.height,
+  },
+  outfitOpacity: {
+    width: 40,
+    height: 40,
+    resizeMode: "contain",
+    opacity: 0.6,
+  },
+  outfitOpacityPants: {
+    width: 35,
+    height: 35,
+    resizeMode: "contain",
+    opacity: 0.6,
+  },
+  items: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textsymbols: {
+    color: Themes.colors.logoGreen,
   },
   topBar: {
     flexDirection: "row",
@@ -157,13 +241,10 @@ const styles = StyleSheet.create({
   },
   //Weather info container
   weatherInfoContainer: {
-    //borderWidth: 1,
     borderRadius: 30,
-    //borderColor: "black",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    //backgroundColor: Themes.colors.weatherOrange,
     width: 250,
     height: 250,
     marginTop: 10,
@@ -202,8 +283,10 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     width: 350,
+    height: windowDimensions.height * 0.2,
+    paddingTop: 15,
     paddingBottom: 10, // Reduced from 20 to 10
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 4 },
@@ -213,29 +296,38 @@ const styles = StyleSheet.create({
   },
   fitCastName: {
     color: Themes.colors.logoGreen,
-    fontSize: 25,
+    fontSize: 28,
     fontWeight: "bold",
-    marginTop: 10,
+    marginBottom: 10,
   },
   fitCastOutfit: {
     marginLeft: 10,
-    marginRight: 20,
+    marginRight: 10,
     marginBottom: 0,
-    //borderColor: "black",
-    //borderWidth: "1",
+    borderwidth: 1,
+    width: "85%",
+    height: "100%",
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
   },
+  line: {
+    width: 1, // Adjust the width of the line
+    backgroundColor: Themes.colors.logoGreen, // Color of the line
+    marginHorizontal: 8, // Adjust the margin as needed
+    height: 80,
+  },
   outfitTop: {
     //marginBottom: 5,
-    width: 80,
-    height: 80,
+    width: 50,
+    height: 50,
     //borderColor: "black",
     //borderWidth: "1",
   },
   outfitBottom: {
-    width: 80,
-    height: 65,
+    width: 50,
+    height: 50,
+    resizeMode: "contain",
     //borderColor: "black",
     //borderWidth: "1",
   },
@@ -248,16 +340,18 @@ const styles = StyleSheet.create({
   fitCastBagItems: {},
   fitCastBagItem: {},
   fitCastDescriptionContainer: {
-    marginTop: 50,
+    marginTop: "25%",
     backgroundColor: Themes.colors.logoGreen,
-    height: "20%",
-    width: "85%",
-    borderRadius: 20,
+    height: "30%",
+    width: "100%",
+    //borderRadius: 20,
+    opacity: 0.9,
     justifyContent: "flex-end", // Align content at the bottom
     alignItems: "center",
-    paddingTop: 15,
+    paddingTop: "8%",
+    paddingHorizontal: 5,
     paddingBottom: 20, // Add padding at the bottom for spacing
-    justifyContent: "center", // Center content vertically
+    justifyContent: "flex-start", // Center content vertically
   },
   fitCastDescriptionSummary: {
     color: Themes.colors.logoYellow,
@@ -276,7 +370,11 @@ const styles = StyleSheet.create({
     alignSelf: "flex-start",
     fontStyle: "italic",
   },
-  fitCastTitleContain: {},
+  fitCastTitleContain: {
+    alignItems: "center",
+    borderwidth: 1,
+    width: "90%",
+  },
   bag: {
     width: 120,
     height: 120,
@@ -286,6 +384,7 @@ const styles = StyleSheet.create({
     //justifyContent: "center", // Adjust this as needed (center, space-between, etc.)
     //alignItems: "center",
     justifyContent: "space-between",
+    alignItems: "center",
     marginTop: 5,
     //borderColor: "black",
     //borderWidth: "1",
